e601fdb64a10ab84ea3f57ccbd02c054a4a7a1c5
Abohte <adinda.bohte@gmail.com>
2018-01-03 15:37:34 +0100

    Add tests for algorithm

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,3 +57,56 @@ RSpec.describe User, type: :model do
     end
   end
 end
+
+describe "pairing algorithm" do
+  let!(:user1) { create :user }
+  let!(:user2) { create :user }
+  let!(:user3) { create :user }
+  let!(:user4) { create :user }
+  let!(:user5) { create :user, admin: true }
+
+  describe "self.set_group" do
+    it "returns an array with all student_ids, no admin_ids" do
+      group = User.set_group.sort
+      expect(group).to eq([user1.id, user2.id, user3.id, user4.id])
+    end
+  end
+
+  describe "self.create_next_group_order" do
+    it "returns an array with all but the first value rotated" do
+      start_group = [1, 2, 3, 4]
+      next_group = User.create_next_group_order(start_group)
+      expect(next_group).to eq([1, 4, 2, 3])
+    end
+  end
+
+  describe "self.generate_pairs" do
+    context "four students, one admin" do
+      it "returns four matches" do
+        start_group = User.set_group
+        User.generate_pairs(start_group)
+        expect(Match.count).to eq(4)
+      end
+      it "returns two pairs" do
+        start_group = User.set_group
+        User.generate_pairs(start_group)
+        expect(Pair.count).to eq(2)
+      end
+    end
+
+    context "five students, one admin" do
+      let!(:user6) { create :user }
+      it "returns four matches" do
+        start_group = User.set_group
+        User.generate_pairs(start_group)
+        expect(Match.count).to eq(5)
+      end
+      it "returns two pairs" do
+        start_group = User.set_group
+        User.generate_pairs(start_group)
+        expect(Pair.count).to eq(2)
+      end
+    end
+  end
+
+end
